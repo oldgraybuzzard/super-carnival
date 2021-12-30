94b51f20e6631404fea5a843cf33d3898f846888
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2021-12-30 10:38:02 -0500

    worked out bugs with indexed db js file, tested and gtg

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,10 @@
-const APP_PREFIX = 'BudgetTracker';     
+const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
   "./index.html",
-  "./public/css/styles.css",
-  "./public/js/index.js"
+  "./css/styles.css",
+  "./js/index.js"
 ];
 
 // Respond with cached resources
